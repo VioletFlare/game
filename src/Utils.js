5ebaef7fed28171f6dc7cb2a887b962c08034ea3
Me <me@example.com>
2018-10-27 17:19:53 +0200

    fix invisible walls when drawn in reverse in tiled

--- src/Utils.js
+++ src/Utils.js
@@ -20,12 +20,30 @@ class Utils {
     
     createInvisibleWalls (context, collisionLayer, invisible = true) {
         let invisibleWalls = context.physics.add.staticGroup();
-    
+
         const calculatePosition = (object) => {
-            return [
-                object.x + object.polyline[0].x, 
-                object.y + object.polyline[0].y
-            ];
+            let position;
+            let isPolylineWidthNegative = object.polyline[1].x < 0;
+            let isPolylineHeightNegative = object.polyline[1].y < 0;
+
+            if (isPolylineWidthNegative) {
+                position = [
+                    (object.x + object.polyline[1].x), 
+                    (object.y + object.polyline[0].y)
+                ]
+            } else if (isPolylineHeightNegative) {
+                position = [
+                    (object.x + object.polyline[0].x), 
+                    (object.y + object.polyline[1].y)
+                ]
+            } else {
+                position = [
+                    (object.x + object.polyline[0].x), 
+                    (object.y + object.polyline[0].y)
+                ]
+            }
+
+            return position;
         }
     
         const calculateDimension = (object, width = 5, height = 5) => {
@@ -33,16 +51,16 @@ class Utils {
     
             isPolyLineHorizontal ? 
                 width = (object.x + object.polyline[1].x) - (object.x + object.polyline[0].x)
-                : 
+            : 
                 height = (object.y + object.polyline[1].y) - (object.y + object.polyline[0].y);
-    
-            return [width, height];
+
+            return [Math.abs(width), Math.abs(height)];
         }
     
         const create = (object) => {
             let position = calculatePosition(object);
             let dimension = calculateDimension(object);
-    
+
             let wall = invisibleWalls.create(...position).setOrigin(0, 0);
             wall.setDisplaySize(...dimension);
     
