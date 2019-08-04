f592a8f71cb20c52d7c515101120df7c7f1e864d
Me <me@example.com>
2019-08-04 21:58:37 +0200

    a fireball flies towards its target

--- src/Ability/Spell/Spell.js
+++ src/Ability/Spell/Spell.js
@@ -6,23 +6,39 @@ class Spell {
 
     preload(scene) {
         this.scene = scene;
-        this.spellContainer = "";
     }
 
     create(atlasName, emitterConfiguration) {
-        const particles = this.scene.add.particles(atlasName);
-        this.emitter = particles.createEmitter(emitterConfiguration);
+        this.particles = this.scene.add.particles(atlasName);
+        this.emitter = this.particles.createEmitter(emitterConfiguration);
     }
 
     cast(user, target) {
 
         if (target) {
-            const trajectory = new Phaser.Geom.Line(user.x, user.y, target.x, target.y),
-            posY = user.y + (user.list[24].y * user.scaleY),
-            posX = user.x + (user.list[24].x * user.scaleX);
+           const originPosY = user.y + (user.list[24].y * user.scaleY),
+            originPosX = user.x + (user.list[24].x * user.scaleX),
+            targetPosY = target.body.center.y,
+            targetPosX = target.body.center.x,
+            originTargetDistance = Phaser.Math.Distance.Between(originPosX, originPosY, targetPosX, targetPosY),
+            angleRad = Phaser.Math.Angle.Between(originPosX, originPosY, targetPosX, targetPosY),
+            angleDeg = Phaser.Math.RadToDeg(angleRad),
+            duration = originTargetDistance * 5,
+            container = 
 
-            this.emitter.setPosition(posX, posY);
+            this.particles.setPosition(originPosX, originPosY);
+            this.particles.setAngle(angleDeg);
+
             this.emitter.start();
+
+            this.scene.tweens.add({
+                targets: this.particles,
+                x: targetPosX, 
+                y: targetPosY,
+                ease: 'Linear',
+                duration: duration
+            });
+
             //use pool
         }
 
