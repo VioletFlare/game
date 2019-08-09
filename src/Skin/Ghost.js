c5dc971edcd02b63f5ebd374ffa07b0880545218
Me <me@example.com>
2019-08-09 08:06:56 +0200

    re-categorize characters as skins

--- src/Skin/Ghost.js
+++ src/Skin/Ghost.js
@@ -1,9 +1,9 @@
 import texPng from "../../assets/ghost/ghost_tex.png";
 import texJson from "../../assets/ghost/ghost_tex.json";
 import skeDbbin from "../../assets/ghost/ghost_ske.dbbin";
-import BaseCharacter from "./BaseCharacter";
+import BaseSkin from "./BaseSkin";
 
-class Ghost extends BaseCharacter {
+class Ghost extends BaseSkin {
 
     constructor() {
         super();
--- src/Skin/Girl.js
+++ src/Skin/Girl.js
@@ -1,9 +1,9 @@
 import texPng from "../../assets/girl/girl_tex.png";
 import texJson from "../../assets/girl/girl_tex.json";
 import skeDbbin from "../../assets/girl/girl_ske.dbbin";
-import BaseCharacter from "./BaseCharacter";
+import BaseSkin from "./BaseSkin";
 
-class Girl extends BaseCharacter {
+class Girl extends BaseSkin {
 
     constructor() {
         super();
