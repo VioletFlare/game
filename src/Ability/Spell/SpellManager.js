3a9c25892018d310082548bfcdfba0b113a4ea30
Me <me@example.com>
2019-08-11 21:20:07 +0200

    optimize player && add spell manager

---
+++ src/Ability/Spell/SpellManager.js
@@ -0,0 +1,25 @@
+class SpellManager {
+
+    cast() {
+
+    }
+    
+    throw(user, target) {
+
+        if (target) { 
+
+        }
+
+    }
+
+    useSpell(user, target, spellConfig) {
+        if (target) {
+            
+        } else {
+            //cast self
+        }
+    }
+
+}
+
+export default SpellManager;
--- src/Player/Player.js
+++ src/Player/Player.js
@@ -6,6 +6,7 @@ class Player extends GameObject {
 
     constructor(config) {
         super(config);
+        this.isNotIdleAnimationBlocked = true;
     }
 
     _run(velocity, flipX) {
@@ -33,42 +34,66 @@ class Player extends GameObject {
         this._createKeyCommands();
     }
 
-    _useAbility() {
-        if (this.config.scene.focusedGameObject) {
-            const effect = new Effect(this.config.scene.abilities.fireball),
-                projectile = new Projectile(this, this.config.scene.focusedGameObject, effect);
+    _useSpell() {
+        const effect = new Effect(this.config.scene.abilities.fireball),
+        projectile = new Projectile(this, this.config.scene.focusedGameObject, effect);
 
-            setTimeout(
-                () => projectile.launch(), 50
-            );
+        setTimeout(
+            () => projectile.launch(), 50
+        );
 
-            this.config.skin.throwSpell(this.armatureDisplay);
+        this.config.skin.throwSpell(this.armatureDisplay);
+
+        setTimeout(
+            () => this.isNotIdleAnimationBlocked = true, 700
+        );
+    }
+
+    _useAbility(abilityConfig) {
+        this.isNotIdleAnimationBlocked = false;
+
+        if (abilityConfig.isSpell) {
+            this._useSpell(abilityConfig.spellConfig);
+        } else if (abilityConfig.isRanged) {
+
+        } else if (abilityConfig.isMeele) {
+
         }
     }
 
     _createKeyCommands() {
-        var spaceKey = this.config.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        const spaceKey = this.config.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE),
+            upKey = this.config.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+
         spaceKey.on('down', 
-            () => this._useAbility()
+            () => this.isNotMoving  ? this._useAbility({
+                isSpell: true,
+                isRanged: true,
+                isMeele: false,
+                spellConfig: {
+                    user: this,
+                    target: this.config.scene.focusedGameObject
+                }
+            }): 0
         );
+
+        upKey.on('down', 
+            () => this.armatureDisplay.body.touching.down ? this._jump(): 0
+        )
     }
 
     update() {
-        const isTouchingDown = this.armatureDisplay.body.touching.down,
-              isNotCasting = !(this.armatureDisplay.animation.lastAnimationName == "cast_0" && this.armatureDisplay.armature.animation.isPlaying),
-              isNotCursorDown = !(this.cursors.right.isDown || this.cursors.left.isDown || this.cursors.up.isDown),
-              isIdle = isTouchingDown && isNotCasting && isNotCursorDown;
+        this.isNotMoving = !(this.cursors.right.isDown || this.cursors.left.isDown || this.cursors.up.isDown) && this.armatureDisplay.body.touching.down,
+        this.isIdle = this.isNotIdleAnimationBlocked && this.isNotMoving;
 
-        if (this.cursors.right.isDown) {
-            this._run(this.config.runVelocity, false)
-        } else if (this.cursors.left.isDown) {
-            this._run(-this.config.runVelocity, true);
-        } else if (isIdle) {
+        if (this.isIdle) {
             this._idle();
-        }
-    
-        if (this.cursors.up.isDown && this.armatureDisplay.body.touching.down) {
-            this._jump();
+        } else {
+            if (this.cursors.right.isDown) {
+                this._run(this.config.runVelocity, false)
+            } else if (this.cursors.left.isDown) {
+                this._run(-this.config.runVelocity, true);
+            }
         }
     }
 
